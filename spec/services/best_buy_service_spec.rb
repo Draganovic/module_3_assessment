63431521da51a81df97c820df0450d952120f083
Admir Draganovic <admir.draganovic@gmail.com>
2016-06-14 11:51:51 -0600

    Response successful

--- spec/services/best_buy_service_spec.rb
+++ spec/services/best_buy_service_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe "Best buy service", type: :request do
     VCR.use_cassette "best_buy_product_search" do
       service = BestBuyService.new
       response = service.stores_search(80202,25)
-      require "pry"; binding.pry
-      expect(response["totalItem"]).to eq(17)
+
+      expect(response[:total]).to eq(17)
+    end
   end
 end
